d5094d1a30dc910de66769a1c19ce11c62f8979e
krisrok <3404365+krisrok@users.noreply.github.com>
2023-08-08 13:35:04 +0200

    Add listener base class

---
+++ Runtime/ContextListenerBase.cs
@@ -0,0 +1,32 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace HierarchyContext
+{
+    [ExecuteAlways]
+    public abstract class ContextListenerBase : MonoBehaviour
+    {
+        [SerializeField, PropertySpace(0, 8)]
+        protected ContextProviderProperty _contextProviderProperty = new ContextProviderProperty();
+
+        protected virtual void OnEnable()
+        {
+            if (_contextProviderProperty.IsInited)
+                return;
+
+            _contextProviderProperty.Init(this, OnContextChanged, allowContextChangedImmediately: true);
+        }
+
+        protected abstract void OnContextChanged(IContextProvider sender, string context);
+
+        protected virtual void OnTransformParentChanged()
+        {
+            _contextProviderProperty.SetParentDirty();
+        }
+
+        protected virtual void OnDestroy()
+        {
+            _contextProviderProperty.Destroy();
+        }
+    }
+}
